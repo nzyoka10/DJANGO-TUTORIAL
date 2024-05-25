7f72c0ef280eb14872f08bb57642981c3c3261fb
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-05-25 14:48:13 +0300

    templates setup

--- members/views.py
+++ members/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
-# my first view.....
 def members(request):
-    return HttpResponse("This is the members page")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
+
+
+
+
+# my first view.....
+# def members(request):
+#     return HttpResponse("This is the members page")
 
 
 
